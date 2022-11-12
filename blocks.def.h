a360011e64c7e64bd03b8d367e7a251e633b54b6
whythefuckamIhere <thereisanimposteramongus@gmail.com>
2022-11-12 16:57:56 +1300

    about to distro hop I think

--- blocks.def.h
+++ blocks.def.h
@@ -2,6 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
+    /*
     {"^b#f38ba8^^c#1e1e2e^ \ufa7d ^d^ ", "~/dwmblocks/scripts/getvolume.sh ", 0, 1},
 
 	{"^b#eba0ac^^c#1e1e2e^ \uf578 ^d^ ", "cat /sys/class/power_supply/BAT0/capacity ",	30,		2},
@@ -9,8 +10,17 @@ static const Block blocks[] = {
 	{"^b#fab387^^c#1e1e2e^ \ue384 ^d^ ", "date +'%a, %d %b, %I:%M %p' ",					30,		3},
 
     {"|", "", 0, 4},
+    */
+
+    {"^c#f38ba8^ \ufa7d ^d^ ", "~/dwmblocks/scripts/getvolume.sh ", 0, 1},
+
+	{"^c#a6e3a1^ \uf578 ^d^ ", "cat /sys/class/power_supply/BAT0/capacity ",	30,		2},
+
+	{"^c#74c7ec^ \ue384 ^d^ ", "date +'%a, %d %b, %I:%M %p' ",					30,		3},
+
+	{" ", "", 0, 4}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " ";
+static char delim[] = " |";
 static unsigned int delimLen = 5;
